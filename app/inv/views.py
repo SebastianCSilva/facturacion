ee5bb2b707340ae65e06811a34e82eef27851956
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2022-09-17 00:37:51 -0300

    Se agrega el poder añadir categorias completo

--- app/inv/views.py
+++ app/inv/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Categoria
+from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
-
+from .forms import CategoriaForm
 
 class CategoriaView(LoginRequiredMixin, generic.ListView):
     model = Categoria
     template_name = "inv/categoria_list.html"
     context_object_name = "obj"
     login_url = 'bases:login'
+
+
+class CategoriaNew(LoginRequiredMixin, generic.CreateView):
+    model = Categoria
+    template_name = "inv/categoria_form.html"
+    context_object_name = "obj"
+    
+    form_class = CategoriaForm
+    success_url = reverse_lazy("inv:categoria_list")
+    login_url = 'bases:login'
+
+    def form_valid(self, form):
+        form.instance.usuario_crea = self.request.user
+        return super().form_valid(form)
