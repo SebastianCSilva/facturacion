264e337291678f7eacdcbf97298cb1d2a288ad7a
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2022-09-20 00:24:31 -0300

    Se agregan las marcas y sus views relacionadas para el funcionamiento CRU

--- app/inv/views.py
+++ app/inv/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Categoria, SubCategoria
+from .models import Categoria, SubCategoria, Marca
 from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
-from .forms import CategoriaForm, SubCategoriaForm
+from .forms import CategoriaForm, SubCategoriaForm, MarcaForm
 
 class CategoriaView(LoginRequiredMixin, generic.ListView):
     model = Categoria
@@ -85,3 +85,38 @@ class SubCategoriaDel(LoginRequiredMixin, generic.DeleteView):
     template_name = 'inv/catalogos_del.html'
     context_object_name = "obj"
     success_url = reverse_lazy("inv:subcategoria_list")
+
+
+class MarcaView(LoginRequiredMixin, generic.ListView):
+    model = Marca
+    template_name = "inv/marca_list.html"
+    context_object_name = "obj"
+    login_url = 'bases:login'
+
+
+class MarcaNew(LoginRequiredMixin, generic.CreateView):
+    model = Marca
+    template_name = "inv/marca_form.html"
+    context_object_name = "obj"
+    
+    form_class = MarcaForm
+    success_url = reverse_lazy("inv:marca_list")
+    login_url = 'bases:login'
+
+    def form_valid(self, form):
+        form.instance.usuario_crea = self.request.user
+        return super().form_valid(form)
+
+
+class MarcaEdit(LoginRequiredMixin, generic.UpdateView):
+    model = Marca
+    template_name = "inv/marca_form.html"
+    context_object_name = "obj"
+    
+    form_class = MarcaForm
+    success_url = reverse_lazy("inv:marca_list")
+    login_url = 'bases:login'
+
+    def form_valid(self, form):
+        form.instance.usuario_modifica = self.request.user.id
+        return super().form_valid(form)
