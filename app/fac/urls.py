c65ee94cab57fadedfe7b19c915ee46b249e2341
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2022-11-16 22:11:16 -0300

    Se crea el listado de clientes

--- app/fac/urls.py
+++ app/fac/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 
-
+from .views import ClienteView
 
 urlpatterns = [
-    #path('/', ProveedorView.as_view(), name='proveedor_list'),
+    path('clientes/', ClienteView.as_view(), name='cliente_list'),
 
 ]
--- app/fac/views.py
+++ app/fac/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from models import Cliente
 
+from bases.views import SinPrivilegios
+from django.views import generic
+
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
 # Create your views here.
+
+
+class ClienteView(LoginRequiredMixin, generic.ListView):
+    model = Cliente
+    template_name = "fac/clientes_list.html"
+    context_object_name = "obj"
+    login_url = 'bases:login'
+    permission_required = "fac.view_cliente"
