4fb8200a0769db1c9acbd4c03cef28e701e5c2bb
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2022-11-18 01:31:03 -0300

    Se crea herencia de Vistas en el sistema de facturacion

--- app/fac/forms.py
+++ app/fac/forms.py
@@ -6,7 +6,15 @@ class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = ['nombres', 'apellidos', 'tipo', 'celular', 'estado']
-        exclude = ['um', 'fm', 'uc', 'fc']
+        exclude = ['usuario_modifica', 'fecha_modificacion', 'usuario_crea', 'fecha_creacion']
+
+    """
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    usuario_crea = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario_modifica = models.IntegerField(blank=True, null=True)
+
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- app/fac/views.py
+++ app/fac/views.py
@@ -3,10 +3,13 @@ from models import Cliente
 
 from bases.views import SinPrivilegios
 from django.views import generic
+from django.urls import reverse_lazy
 
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required, permission_required
+
+from .forms import ClienteForm
 # Create your views here.
 
 
@@ -16,3 +19,36 @@ class ClienteView(LoginRequiredMixin, generic.ListView):
     context_object_name = "obj"
     login_url = 'bases:login'
     permission_required = "fac.view_cliente"
+
+
+class VistaBaseCreate(SuccessMessageMixin, SinPrivilegios, generic.CreateView):
+    context_object_name = "obj"
+    success_message = "Registro Agregado Satisfactoriamente"
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
+
+
+class VistaBaseEdit(SuccessMessageMixin, SinPrivilegios, generic.UpdateView):
+    context_object_name = "obj"
+    success_message = "Registro Actualizado Satisfactoriamente"
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
+
+
+class ClienteNew(VistaBaseCreate):
+    model = Cliente
+    template_name = "fac/cliente_form.html"
+    form_class = ClienteForm
+    success_url = reverse_lazy("fac:cliente_list")
+    permission_required = "fac.add_cliente"
+
+class ClienteEdit(VistaBaseEdit):
+    model = Cliente
+    template_name = "fac/cliente_form.html"
+    form_class = ClienteForm
+    success_url = reverse_lazy("fac:cliente_list")
+    permission_required = "fac.change_cliente"
