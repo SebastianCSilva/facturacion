0db019046b3d6ef166af96db5570233d5c185287
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2022-11-21 23:16:35 -0300

    Se modifica errores del modelo opcional

--- app/bases/models.py
+++ app/bases/models.py
@@ -13,3 +13,16 @@ class ClaseModelo(models.Model):
 
     class Meta:
         abstract=True
+
+
+class ClaseModelo2(models.Model):
+    estado = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    #usuario_crea = models.ForeignKey(User, on_delete=models.CASCADE)
+    #usuario_modifica = models.IntegerField(blank=True, null=True)
+    usuario_crea = UserForeignKey(auto_user_add=True, related_name='+')
+    usuario_modifica = UserForeignKey(auto_user_add=True, related_name='+')
+
+    class Meta:
+        abstract=True
