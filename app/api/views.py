079a5dfb170db73c491642b6dad1395d5435c41e
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2022-12-01 23:24:44 -0300

    se agregan los EndPoint Productos View

--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_list_or_404
+
+from .serializers import ProductoSerializer
+from inv.models import Producto
+
+class ProductoList(APIView):
+    """Retorno de todos los productos"""
+    def get(self, request):
+        prod = Producto.objects.all()
+        data = ProductoSerializer(prod, many=True).data
+        return Response(data)
+
+
+class ProductoDetalle(APIView):
+    """Retorno de producto individual"""
+    def get(self, request, codigo):
+        prod = get_list_or_404(Producto, codigo=codigo)
+        data = ProductoSerializer(prod).data
+        return Response(data)
