ad3bdce9a3df8e15838f190faae3e798b462da1e
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2022-12-06 00:05:17 -0300

    Se agrega metodo POST para guerdar las Facturas

--- app/fac/urls.py
+++ app/fac/urls.py
@@ -6,11 +6,12 @@ urlpatterns = [
     path('clientes/', ClienteView.as_view(), name='cliente_list'),
     path('clientes/new', ClienteNew.as_view(), name='cliente_new'),
     path('clientes/<int:pk>', ClienteEdit.as_view(), name='cliente_edit'),
-    #
     path('clientes/estado/<int:id>', clienteInactivar, name='cliente_inactivar'),
-
+    #
     path('facturas/', FacturaView.as_view(), name='factura_list'),
     path('facturas/new', facturas, name='factura_new'),
+    path('facturas/edit/<int:pk>', facturas, name='factura_edit'),
+    #
     path('facturas/buscar-producto', ProductoView.as_view(), name='factura_producto'),
 
 ]
