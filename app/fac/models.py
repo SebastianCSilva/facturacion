4787df6e5b301ce878a122ca513c0bb0bbe629d6
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2022-12-08 00:46:19 -0300

    Se crean permisos personalizados para facturas enc y det

--- app/fac/models.py
+++ app/fac/models.py
@@ -49,6 +49,9 @@ class FacturaEnc(ClaseModelo2):
     class Meta:
         verbose_name_plural = "Encabezado Facturas"
         verbose_name = "Encabezado Factura"
+        permissions = [
+            ("sup_caja_facturaenc", "Permisos de Supervisor de Caja Encabezado"),
+        ]
 
 class FacturaDet(ClaseModelo2):
     factura = models.ForeignKey(FacturaEnc, on_delete=models.CASCADE)
@@ -70,6 +73,9 @@ class FacturaDet(ClaseModelo2):
     class Meta:
         verbose_name_plural = "Detalles Facturas"
         verbose_name = "Detalle Factura"
+        permissions = [
+            ("sup_caja_facturadet", "Permisos de Supervisor de Caja Detalle"),
+        ]
 
 
 @receiver(post_save, sender=FacturaDet)
