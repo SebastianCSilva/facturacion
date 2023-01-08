c5eba61df369331862fb881274b5304068f9c461
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2023-01-07 22:43:25 -0300

    Se crea e implementa el imprimir facturas

--- app/fac/urls.py
+++ app/fac/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 
 from .views import ClienteView, ClienteNew, ClienteEdit, clienteInactivar, FacturaView, facturas, ProductoView, borrar_detalle_factura
 
+from reportes import imprimir_factura_recibo
+
 urlpatterns = [
     path('clientes/', ClienteView.as_view(), name='cliente_list'),
     path('clientes/new', ClienteNew.as_view(), name='cliente_new'),
@@ -14,5 +16,7 @@ urlpatterns = [
     #
     path('facturas/buscar-producto', ProductoView.as_view(), name='factura_producto'),
     path('facturas/borrar-detalle/<int:id>', borrar_detalle_factura, name='factura_borrar_detalle'),
+    #
+    path('facturas/imprimir/<int:id>', imprimir_factura_recibo, name='factura_imprimir_one'),
 
 ]
